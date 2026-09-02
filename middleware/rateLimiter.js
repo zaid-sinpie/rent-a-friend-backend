cfefbd279a36f73047bd64c6ffa21122db0da155
zaid-sinpie <zaidmasuldar@gmail.com>
2026-09-02 21:34:03 +0530

    increase limiter for login

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -13,7 +13,7 @@ const signupLimiter = rateLimit({
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 2,
+  max: 10,
   message: {
     success: false,
     message: "Too many login attempts. Please try again later.",
